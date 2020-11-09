7a7b05c9085edb0f324092ce1d537f69ad48095b
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-11-09 11:07:27 +0700

    ident

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 a = int(input("Выберете ваше семейное положение. 1 - одинок(-а), 2 - состою в браке. 3 - родитель-одиночка."))
-if a == 1:
+name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
+
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format(QUESTION,name_month[month], end =''))
+    income = float(input())
+    annual_income += income
+print(annual_income)
