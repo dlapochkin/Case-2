9763c80b80b1964d50efd79299dc8b0f7d075607
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-11-09 11:41:50 +0700

    ident

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 a = int(input("Выберете ваше семейное положение. 1 - одинок(-а), 2 - состою в браке. 3 - родитель-одиночка."))
-b = int(input("Введите ваш годовой доход"))
-c = int(input("Введите ваш налоговый вычет"))
+Question = (input("Введите ваш налоговый вычет"))
 
+name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
+
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format(Question,name_month[month], end =''))
+    income = float(input())
+    annual_income += income
+print(annual_income)
