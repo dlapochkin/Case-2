f100aef8fa8c3e134e959578cb6d1d829512810c
dlapochkin <d.lapochkin@g.nsu.ru>
2020-11-09 10:21:08 +0700

    first

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-a = int(input("Выберете ваше семейное положение. 1 - одинок(-а), 2 - состою в браке. 3 - родитель-одиночка."))
+br = int(input("Выберете ваше семейное положение. 1 - одинок(-а), 2 - состою в браке. 3 - родитель-одиночка."))
 name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 QUESTION = 'сколько вы заработали в'
 annual_income = 0
@@ -8,3 +8,7 @@ for month in range(12):
     annual_income += income
 c = int(input("Введите ваш налоговый вычет"))
 t = annual_income - c  # taxable income
+
+if br == 1:
+    numbers = [9075, 36900, 89350, 186350, 405100, 406750]
+    a, b, c, d, e, f = list(map(int, numbers))
