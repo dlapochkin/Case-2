d9b050bcd4c7c10daa73c9216b1b78926a89f38d
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-11-09 15:30:41 +0700

    r

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ for month in range(12):
 r = int(input("Введите ваш налоговый вычет"))
 t = annual_income - r  # taxable income
 
-if br == 1:
+if br == '1':
     numbers = [9075, 36900, 89350, 186350, 405100, 406750]
     a, b, c, d, e, f = list(map(int, numbers))
-if br == 2:
+if br == '2':
     numbers = [18150, 73800, 148850, 226850, 405100, 457600]
     a, b, c, d, e, f = list(map(int, numbers))
-if br == 3:
+if br == '3':
     numbers = [12950, 49400, 127550, 206600, 405100, 432200]
     a, b, c, d, e, f = list(map(int, numbers))
 if 0 < t <= a:
