d8a327f51cd0181f3e7b662dd567e0da45d70abb
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-11-09 11:46:23 +0700

    input

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 a = int(input("Выберете ваше семейное положение. 1 - одинок(-а), 2 - состою в браке. 3 - родитель-одиночка."))
-b = int(input("Введите ваш годовой доход"))
+name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+QUESTION ='сколько вы заработали в'
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format(QUESTION,name_month[month], end =''))
+    income = float(input())
+    annual_income += income
+b = annual_income
 c = int(input("Введите ваш налоговый вычет"))
-
