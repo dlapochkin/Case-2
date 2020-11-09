ee62f9a70b821e9718affc11a6524c41bdc80008
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-11-09 11:23:02 +0700

    income

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # string constants
 name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
-
+QUESTION ='сколько вы зарабатываете в'
 annual_income = 0
 for month in range(12):
-    print('{} {}:'.format(QUESTION,name_month[month], end ='')
+    print('{} {}:'.format(QUESTION,name_month[month], end =''))
     income = float(input())
     annual_income += income
 print(annual_income)
