c957bb471f3e7d8b28faa8fae3e1c48dc99d7c74
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-11-09 11:39:08 +0700

    income

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # string constants
-name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
-QUESTION ='сколько вы зарабатываете в'
+name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+QUESTION='сколько вы заработали в'
 annual_income = 0
 for month in range(12):
     print('{} {}:'.format(QUESTION,name_month[month], end =''))
